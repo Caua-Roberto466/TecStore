999cbfd8c914f696fe9945ad0ebad143b719f27b
Cauã Roberto <cauaroberto211@gmail.com>
2026-08-24 20:05:01 -0300

    Adicionando os arquivos

---
+++ src/ui/TelaCadastro.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javax.swing.*;
+
+public class TelaCadastro extends JFrame {
+    public TelaCadastro(){
+
+    }
+
+    public void componentes(){
+        JLabel rtlNome, rtlEmail, rtlSenha, titulo;
+        JTextField txtNome, txtEmail, txtSenha;
+        JButton cadastrar;
+    }
+}
